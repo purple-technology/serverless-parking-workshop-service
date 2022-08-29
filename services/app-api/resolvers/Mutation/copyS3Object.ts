c617c9ab516d57e6c727203dac9518ef76dbda00
Filip Pýrek <PyrekFilip@gmail.com>
2022-08-29 16:08:15 +0200

    feat: fix copys3objects API

--- services/app-api/resolvers/Mutation/copyS3Object.ts
+++ services/app-api/resolvers/Mutation/copyS3Object.ts
@@ -1,9 +1,22 @@
-import { Mutation, MutationCopyS3ObjectArgs } from '@packages/app-graphql-types'
+import {
+	Camera,
+	Mutation,
+	MutationCopyS3ObjectArgs
+} from '@packages/app-graphql-types'
 import { AppSyncIdentityCognito, AppSyncResolverHandler } from 'aws-lambda'
 import AWS from 'aws-sdk'
 
 const s3 = new AWS.S3()
 const dynamoDb = new AWS.DynamoDB()
+
+const cameraMap: { [objectId: string]: Camera } = {
+	'1': Camera.Entrance,
+	'2': Camera.Entrance,
+	'3': Camera.Exit,
+	'4': Camera.Exit,
+	'5': Camera.ParkingLot,
+	'6': Camera.ParkingLot
+}
 
 export const handler: AppSyncResolverHandler<
 	MutationCopyS3ObjectArgs,
@@ -33,10 +46,18 @@ export const handler: AppSyncResolverHandler<
 	}
 
 	try {
+		const camera = cameraMap[event.arguments.objectId]
+
+		if (typeof camera === 'undefined') {
+			throw new Error(
+				`Object with objectId="${event.arguments.objectId}" not found`
+			)
+		}
+
 		await s3
 			.copyObject({
 				Bucket: data.Item.s3BucketName.S,
-				Key: `${Date.now()}.jpg`,
+				Key: `${camera}/${Date.now()}.jpg`,
 				CopySource: `${process.env.PHOTOS_BUCKET_NAME}/${event.arguments.objectId}.jpg`
 			})
 			.promise()
